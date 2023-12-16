8816bfb3cb35549299965de332534a76f8e5cbe9
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-16 18:57:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// Assume this function is defined in your index.js file
+function writeCards(names, action) {
+    const thankYouNotes = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${action} gift!`;
+      thankYouNotes.push(message);
+    }
+    return thankYouNotes;
+  }
+function countDown(firstNumber) {
+    for (let i = firstNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
